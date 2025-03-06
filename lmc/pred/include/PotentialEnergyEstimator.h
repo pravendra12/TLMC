228e2b9c008964ddf033b49f93a49873626848ef
pravendra12 <105543056+pravendra12@users.noreply.github.com>
2025-03-06 19:14:06 +0000

    Parallel adding sites to existing cluster

--- lmc/pred/include/PotentialEnergyEstimator.h
+++ lmc/pred/include/PotentialEnergyEstimator.h
@@ -1,33 +1,32 @@
-/**************************************************************************************************
- * Copyright (c) 2022-2023. All rights reserved.                                                  *
- * @Author: Zhucong Xi                                                                            *
- * @Date: 11/15/22 12:36 PM                                                                       *
- * @Last Modified by: pravendra12                                                                 *
- * @Last Modified time: 11/30/24 5:00 PM                                                          *
- **************************************************************************************************/
-
-/*! \file  PotentialEnergyEstimator.h
- *  \brief File for the PotentialEnergyEstimator class definition.
- */
 #ifndef LMC_PRED_INCLUDE_POTENTIALENERGYESTIMATOR_H_
 #define LMC_PRED_INCLUDE_POTENTIALENERGYESTIMATOR_H_
 
 #include <set>
 #include <numeric>
 #include <algorithm>
-
-#include "eigen3/Eigen/Dense"
+#include <eigen3/Eigen/Dense>
 #include "ClusterExpansion.h"
 #include "JsonUtility.h"
+using namespace std;
+using namespace Eigen;
 
-/*! \brief Class for defining cluster expansion Hamiltonian.
+/*!
+ * \file PotentialEnergyEstimator.h
+ * \brief File for the PotentialEnergyEstimator class definition.
+ * 
+ * \author zhucongx
+ * \date 6th March 2025
+ * \lastedited 6th March 2025 by pravendra12
+ */
+
+/*! \brief Class for defining Cluster Expansion Hamiltonian.
  */
 class PotentialEnergyEstimator {
  public:
-  PotentialEnergyEstimator(const std::string &predictor_filename,
+  PotentialEnergyEstimator(const string &predictor_filename,
                            const Config &reference_config,
                            const Config &supercell_config,
-                           const std::set<Element> &element_set,
+                           const set<Element> &element_set,
                            size_t max_cluster_size,
                            size_t max_bond_order);
   ~PotentialEnergyEstimator();
@@ -37,7 +36,7 @@ class PotentialEnergyEstimator {
    *  \param config   The configuration the code works on
    *  \return         The encode vector
    */  
-  [[nodiscard]] Eigen::VectorXd GetEncodeVector(const Config &config) const;
+  [[nodiscard]] VectorXd GetEncodeVector(const Config &config) const;
   
   /*! \brief Get the encoded vector of the configuration, representing the count
    *         of each cluster type, including void clusters.
@@ -45,8 +44,8 @@ class PotentialEnergyEstimator {
    *  \param lattice_cluster  Vector containing lattice site IDs for the cluster.
    *  \return                 A vector representing the encoded cluster type counts.
    */
-  [[nodiscard]] Eigen::VectorXd GetEncodeVectorOfCluster(const Config &config, 
-                                                        std::vector<size_t> cluster) const;
+  [[nodiscard]] VectorXd GetEncodeVectorOfCluster(const Config &config, 
+                                                        vector<size_t> cluster) const;
 
   /*! \brief Get the Energy of the configuration
    *  \param config           The configuration to analyze.
@@ -60,33 +59,33 @@ class PotentialEnergyEstimator {
    *  \return                 Energy of the cluster.
    */
   [[nodiscard]] double GetEnergyOfCluster(const Config &config,
-                                          const std::vector<size_t> &cluster) const;
+                                          const vector<size_t> &cluster) const;
   
   /*! \brief Get the energy change due to atom jump.
    *  \param config           The configuration to analyze.
    *  \param lattice_id_pair  Lattice Id jump pair.
    *  \return                 Change in energy due to atom jump.
    */
-  [[nodiscard]] double GetDe(Config &config, const std::pair<size_t, size_t> &lattice_id_pair) const;
-  [[nodiscard]] std::map<Element, double> GetChemicalPotential(Element solvent_element) const;
+  [[nodiscard]] double GetDe(Config &config, const pair<size_t, size_t> &lattice_id_pair) const;
+  [[nodiscard]] map<Element, double> GetChemicalPotential(Element solvent_element) const;
   
  private:
-  const std::pair<Eigen::VectorXd, double> ce_fitted_parameters_{};
+  const pair<VectorXd, double> ce_fitted_parameters_{};
   /// Adjusted Effective cluster interaction.
-  const Eigen::VectorXd adjusted_beta_ce_{};
+  const VectorXd adjusted_beta_ce_{};
 
   /// @brief Adjusted Intercept
   const double adjusted_intercept_ce_;
  
   /// Element set
-  const std::set<Element> element_set_{};
+  const set<Element> element_set_{};
 
   /// Set that contains all available cluster types
-  const std::set<ClusterType> initialized_cluster_type_set_{};   
+  const set<ClusterType> initialized_cluster_type_set_{};   
 
   /// Maps each lattice cluster type to its count. 
   /// Used mainly for configuration used for training Cluster Expansion Model.
-  const std::unordered_map<LatticeClusterType, size_t, 
+  const unordered_map<LatticeClusterType, size_t, 
                 boost::hash<LatticeClusterType>> lattice_cluster_type_count_{};
   
   /// Maximum Cluster Size

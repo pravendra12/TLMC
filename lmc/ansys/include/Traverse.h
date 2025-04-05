0adc3dfcd6da64afd7d593116e66ddf48ec6680d
pravendra12 <105543056+pravendra12@users.noreply.github.com>
2025-04-05 03:22:34 +0000

    Parallel version of Traverse
    Batch wise ansys of configuration

--- lmc/ansys/include/Traverse.h
+++ lmc/ansys/include/Traverse.h
@@ -1,9 +1,11 @@
 #ifndef LMC_LMC_ANSYS_INCLUDE_TRAVERSE_H_
 #define LMC_LMC_ANSYS_INCLUDE_TRAVERSE_H_
+
+#include <omp.h>
+#include <nlohmann/json.hpp>
 #include "ShortRangeOrder.h"
 #include "B2OrderParameter.h"
 
-#include <nlohmann/json.hpp>
 
 namespace ansys {
 
--- lmc/ansys/src/Traverse.cpp
+++ lmc/ansys/src/Traverse.cpp
@@ -9,204 +9,315 @@
 #include <iostream>
 #include <omp.h>
 
-namespace ansys {
-static Config GetConfig(const std::string &config_type, size_t i, std::vector<double> cutoff) {
-  Config config;
-  if (config_type == "config") {
-    config = Config::ReadConfig(std::to_string(i) + ".cfg");
-    /////////////////////
-    config.UpdateNeighborList(cutoff);
-  } else {
-    throw std::invalid_argument("Unknown config type: " + config_type);
-  }
-  return config;
-}
-
-Traverse::Traverse(unsigned long long int initial_steps,
-                   unsigned long long int increment_steps,
-                   std::vector<double> cutoffs,
-                   std::string log_type,
-                   std::string config_type)
-    : initial_steps_(initial_steps),
-      increment_steps_(increment_steps),
-      final_steps_(increment_steps),
-      cutoffs_(std::move(cutoffs)),
-      log_type_(std::move(log_type)),
-      config_type_(std::move(config_type)),
-      log_map_{},
-      frame_ofs_("ansys_frame_log.txt", std::ofstream::out) {
-
-  std::string log_file_name;
-  if (log_type_ == "kinetic_mc") {
-    log_file_name = "kmc_log.txt";
-  } else if (log_type_ == "canonical_mc") {
-    log_file_name = "cmc_log.txt";
-  } else if (log_type_ == "simulated_annealing") {
-    log_file_name = "sa_log.txt";
-  } else {
-    throw std::invalid_argument("Unknown log type: " + log_type_);
-  }
-
-  std::ifstream ifs(log_file_name, std::ifstream::in);
-  if (!ifs.is_open()) {
-    throw std::runtime_error("Cannot open " + log_file_name);
-  }
-  while (ifs.peek() == '#') {
-    ifs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  }
-
-  std::string buffer;
-  // read header
-  std::getline(ifs, buffer);
-  std::vector<std::string> headers;
-  boost::algorithm::split(headers, buffer, boost::is_any_of("\t"));
-
-  // read data
-  while (std::getline(ifs, buffer)) {
-    if (buffer.empty()) {
-      continue;
+namespace ansys
+{
+  static Config GetConfig(const std::string &config_type, size_t i, std::vector<double> cutoff)
+  {
+    Config config;
+    if (config_type == "config")
+    {
+      config = Config::ReadConfig(std::to_string(i) + ".cfg");
+      /////////////////////
+      config.UpdateNeighborList(cutoff);
     }
-    if (buffer[0] == '#') {
-      continue;
+    else
+    {
+      throw std::invalid_argument("Unknown config type: " + config_type);
     }
-    std::istringstream line_stream(buffer);
-    unsigned long long step_number;
-    line_stream >> step_number;
-    if (step_number < initial_steps_ || (step_number - initial_steps_) % increment_steps != 0) {
-      continue;
+    return config;
+  }
+
+  Traverse::Traverse(unsigned long long int initial_steps,
+                     unsigned long long int increment_steps,
+                     std::vector<double> cutoffs,
+                     std::string log_type,
+                     std::string config_type)
+      : initial_steps_(initial_steps),
+        increment_steps_(increment_steps),
+        final_steps_(increment_steps),
+        cutoffs_(std::move(cutoffs)),
+        log_type_(std::move(log_type)),
+        config_type_(std::move(config_type)),
+        log_map_{},
+        frame_ofs_("ansys_frame_log.txt", std::ofstream::out)
+  {
+
+    std::string log_file_name;
+    if (log_type_ == "kinetic_mc")
+    {
+      log_file_name = "kmc_log.txt";
     }
-    final_steps_ = step_number;
-    size_t col_index = 1;
-    while (line_stream >> buffer) {
-      const auto &key = headers[col_index];
-      try {
-        const auto double_value = boost::lexical_cast<double>(buffer);
-        if (!std::holds_alternative<std::unordered_map<unsigned long long, double>>(log_map_[key])) {
-          log_map_[key] = std::unordered_map<unsigned long long, double>();
-        }
-        std::get<std::unordered_map<unsigned long long, double>>(log_map_[key])[step_number] = double_value;
-      } catch (const boost::bad_lexical_cast &) {
-        if (!std::holds_alternative<std::unordered_map<unsigned long long, std::string>>(log_map_[key])) {
-          log_map_[key] = std::unordered_map<unsigned long long, std::string>();
-        }
-        std::get<std::unordered_map<unsigned long long, std::string>>(log_map_[key])[step_number] = buffer;
+    else if (log_type_ == "canonical_mc")
+    {
+      log_file_name = "cmc_log.txt";
+    }
+    else if (log_type_ == "simulated_annealing")
+    {
+      log_file_name = "sa_log.txt";
+    }
+    else
+    {
+      throw std::invalid_argument("Unknown log type: " + log_type_);
+    }
+
+    std::ifstream ifs(log_file_name, std::ifstream::in);
+    if (!ifs.is_open())
+    {
+      throw std::runtime_error("Cannot open " + log_file_name);
+    }
+    while (ifs.peek() == '#')
+    {
+      ifs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    std::string buffer;
+    // read header
+    std::getline(ifs, buffer);
+    std::vector<std::string> headers;
+    boost::algorithm::split(headers, buffer, boost::is_any_of("\t"));
+
+    // read data
+    while (std::getline(ifs, buffer))
+    {
+      if (buffer.empty())
+      {
+        continue;
       }
-      col_index++;
+      if (buffer[0] == '#')
+      {
+        continue;
+      }
+      std::istringstream line_stream(buffer);
+      unsigned long long step_number;
+      line_stream >> step_number;
+      if (step_number < initial_steps_ || (step_number - initial_steps_) % increment_steps != 0)
+      {
+        continue;
+      }
+      final_steps_ = step_number;
+      size_t col_index = 1;
+      while (line_stream >> buffer)
+      {
+        const auto &key = headers[col_index];
+        try
+        {
+          const auto double_value = boost::lexical_cast<double>(buffer);
+          if (!std::holds_alternative<std::unordered_map<unsigned long long, double>>(log_map_[key]))
+          {
+            log_map_[key] = std::unordered_map<unsigned long long, double>();
+          }
+          std::get<std::unordered_map<unsigned long long, double>>(log_map_[key])[step_number] = double_value;
+        }
+        catch (const boost::bad_lexical_cast &)
+        {
+          if (!std::holds_alternative<std::unordered_map<unsigned long long, std::string>>(log_map_[key]))
+          {
+            log_map_[key] = std::unordered_map<unsigned long long, std::string>();
+          }
+          std::get<std::unordered_map<unsigned long long, std::string>>(log_map_[key])[step_number] = buffer;
+        }
+        col_index++;
+      }
     }
-  }
+
+    std::cout << "Initial Steps: " << initial_steps_ << std::endl;
+    std::cout << "Increment Steps: " << increment_steps_ << std::endl;
+    std::cout << "Final Steps: " << final_steps_ << std::endl;
 
 #pragma omp parallel default(none) shared(std::cout)
-  {
+    {
 #pragma omp master
-    { std::cout << "Using " << omp_get_num_threads() << " threads." << std::endl; }
+      {
+        std::cout << "Using " << omp_get_num_threads() << " threads." << std::endl;
+      }
+    }
   }
-}
 
-Traverse::~Traverse() = default;
+  Traverse::~Traverse() = default;
 
-void Traverse::RunAnsys() const {
-
-  std::set<Element>element_set;
-  std::cout << "Initial Steps: " << initial_steps_ << std::endl;
-  std::cout << "Increment Steps: " << increment_steps_ << std::endl;
-  std::cout << "Final Steps: " << final_steps_ << std::endl;
-
-  for (unsigned long long i = initial_steps_; i <= final_steps_; i+= increment_steps_) 
+  // Parallel Version
+  void Traverse::RunAnsys() const
   {
-    
-    // reading the config
-    auto config = GetConfig(config_type_, i, cutoffs_);
-    
-    if (element_set.empty()) {
+    std::set<Element> element_set;
+    std::vector<unsigned long long> config_indices;
+
+    // Generate config indices
+    for (unsigned long long i = initial_steps_; i <= final_steps_; i += increment_steps_)
+    {
+      config_indices.push_back(i);
+    }
+
+    int total_configs = static_cast<int>(config_indices.size());
+    int num_threads = omp_get_max_threads();
+
+    // Write header once
+    {
+      auto config = GetConfig(config_type_, config_indices[0], cutoffs_);
       auto atomVector = config.GetAtomVector();
       element_set = std::set<Element>(atomVector.begin(), atomVector.end());
-      Element vacancy("X");
-      element_set.erase(vacancy);
-
+      element_set.erase(Element("X"));
       frame_ofs_ << GetHeaderFrameString(element_set) << std::flush;
     }
 
+    // Process in batches
+    for (int batch_start = 0; batch_start < total_configs; batch_start += num_threads)
+    {
+      int batch_end = std::min(batch_start + num_threads, total_configs);
+      std::vector<std::ostringstream> output_buffers(batch_end - batch_start);
 
-    const auto time = log_map_.find("time") == log_map_.end()
-        ? nan("")
-        : std::get<std::unordered_map<unsigned long long, double>>(log_map_.at("time")).at(i);
+// Parallel region
+#pragma omp parallel for num_threads(num_threads)
+      for (int idx = batch_start; idx < batch_end; ++idx)
+      {
+        int local_index = idx - batch_start;
+        unsigned long long i = config_indices[idx];
+        auto config = GetConfig(config_type_, i, cutoffs_);
 
-    const auto temperature = std::get<std::unordered_map<unsigned long long, double>>(log_map_.at("temperature")).at(i);
+        const auto time = log_map_.find("time") == log_map_.end()
+                              ? nan("")
+                              : std::get<std::unordered_map<unsigned long long, double>>(log_map_.at("time")).at(i);
 
-    const auto energy = std::get<std::unordered_map<unsigned long long, double>>(log_map_.at("energy")).at(i);
+        const auto temperature = std::get<std::unordered_map<unsigned long long, double>>(log_map_.at("temperature")).at(i);
+        const auto energy = std::get<std::unordered_map<unsigned long long, double>>(log_map_.at("energy")).at(i);
 
-    frame_ofs_ << i << "\t" << time << "\t" << temperature << "\t" << energy;
+        std::ostringstream &oss = output_buffers[local_index];
+        oss << i << "\t" << time << "\t" << temperature << "\t" << energy;
 
-    // sro information
-    ShortRangeOrder short_range_order(config, element_set);
-    const auto sro1 = short_range_order.FindWarrenCowley(1);
-    
-    const auto sro2 = short_range_order.FindWarrenCowley(2);
-  
-    const auto sro3 = short_range_order.FindWarrenCowley(3);
-    
-    for (const auto& pair : sro1) {
+        ShortRangeOrder short_range_order(config, element_set);
+        const auto sro1 = short_range_order.FindWarrenCowley(1);
+        const auto sro2 = short_range_order.FindWarrenCowley(2);
+        const auto sro3 = short_range_order.FindWarrenCowley(3);
 
-      double sro1_value = sro1.count(pair.first) ? sro1.at(pair.first) : nan("");
-      double sro2_value = sro2.count(pair.first) ? sro2.at(pair.first) : nan("");
-      double sro3_value = sro3.count(pair.first) ? sro3.at(pair.first) : nan("");
-      frame_ofs_ << "\t" << sro1_value << "\t" << sro2_value << "\t" << sro3_value;
-      
+        for (const auto &pair : sro1)
+        {
+          double sro1_value = sro1.count(pair.first) ? sro1.at(pair.first) : nan("");
+          double sro2_value = sro2.count(pair.first) ? sro2.at(pair.first) : nan("");
+          double sro3_value = sro3.count(pair.first) ? sro3.at(pair.first) : nan("");
+          oss << "\t" << sro1_value << "\t" << sro2_value << "\t" << sro3_value;
+        }
+
+        B2OrderParameter b2Order(config);
+        for (auto element : element_set)
+        {
+          double b2OrderParameter = b2Order.GetB2OrderParameter(element);
+          double alphaOccupancy = b2Order.GetAlphaSiteOccupancy(element);
+          double betaOccupancy = b2Order.GetBetaSiteOccupancy(element);
+          oss << "\t" << b2OrderParameter << "\t" << alphaOccupancy << "\t" << betaOccupancy;
+        }
+
+        oss << "\n";
+      }
+
+      // Write the batch to file in order
+      for (auto &oss : output_buffers)
+      {
+        frame_ofs_ << oss.str();
+      }
     }
 
+    frame_ofs_.close();
+  }
+
+  /*
+  void Traverse::RunAnsys() const
+  {
+
+    std::set<Element> element_set;
+
+    for (unsigned long long i = initial_steps_; i <= final_steps_; i += increment_steps_)
+    {
+      // reading the config
+      auto config = GetConfig(config_type_, i, cutoffs_);
+
+      if (element_set.empty())
+      {
+        auto atomVector = config.GetAtomVector();
+        element_set = std::set<Element>(atomVector.begin(), atomVector.end());
+        Element vacancy("X");
+        element_set.erase(vacancy);
+
+        frame_ofs_ << GetHeaderFrameString(element_set) << std::flush;
+      }
+
+      const auto time = log_map_.find("time") == log_map_.end()
+                            ? nan("")
+                            : std::get<std::unordered_map<unsigned long long, double>>(log_map_.at("time")).at(i);
+
+      const auto temperature = std::get<std::unordered_map<unsigned long long, double>>(log_map_.at("temperature")).at(i);
+
+      const auto energy = std::get<std::unordered_map<unsigned long long, double>>(log_map_.at("energy")).at(i);
+
+      frame_ofs_ << i << "\t" << time << "\t" << temperature << "\t" << energy;
+
+      // sro information
+      ShortRangeOrder short_range_order(config, element_set);
+      const auto sro1 = short_range_order.FindWarrenCowley(1);
+
+      const auto sro2 = short_range_order.FindWarrenCowley(2);
+
+      const auto sro3 = short_range_order.FindWarrenCowley(3);
+
+      for (const auto &pair : sro1)
+      {
+
+        double sro1_value = sro1.count(pair.first) ? sro1.at(pair.first) : nan("");
+        double sro2_value = sro2.count(pair.first) ? sro2.at(pair.first) : nan("");
+        double sro3_value = sro3.count(pair.first) ? sro3.at(pair.first) : nan("");
+        frame_ofs_ << "\t" << sro1_value << "\t" << sro2_value << "\t" << sro3_value;
+      }
+
+      // B2 Order Parameter
+
+      B2OrderParameter b2Order(config);
+
+      for (auto element : element_set)
+      {
+        double b2OrderParameter = b2Order.GetB2OrderParameter(element);
+        double alphaOccupancy = b2Order.GetAlphaSiteOccupancy(element);
+        double betaOccupancy = b2Order.GetBetaSiteOccupancy(element);
+
+        frame_ofs_ << "\t" << b2OrderParameter
+                   << "\t" << alphaOccupancy
+                   << "\t" << betaOccupancy;
+      }
+
+      frame_ofs_ << "\n";
+    }
+    frame_ofs_.close();
+  }
+*/
+  std::string Traverse::GetHeaderFrameString(const std::set<Element> &element_set) const
+  {
+    std::string header_frame = "steps\ttime\ttemperature\tenergy\t";
+
+    // SRO Parameter
+    static const std::vector<std::string> order_list{"first", "second", "third"};
+    for (auto element1 : element_set)
+    {
+      for (auto element2 : element_set)
+      {
+        for (const auto &order : order_list)
+        {
+          header_frame += "warren_cowley_" + order + "_" + element1.GetElementString() + "-" + element2.GetElementString() + "\t";
+        }
+      }
+    }
 
     // B2 Order Parameter
 
-    B2OrderParameter b2Order(config);
-
     for (auto element : element_set)
     {
-      double b2OrderParameter = b2Order.GetB2OrderParameter(element);
-      double alphaOccupancy = b2Order.GetAlphaSiteOccupancy(element);
-      double betaOccupancy = b2Order.GetBetaSiteOccupancy(element);
-
-      frame_ofs_ << "\t" << b2OrderParameter 
-                 << "\t" << alphaOccupancy 
-                 << "\t" << betaOccupancy;
+      auto elementString = element.GetElementString();
+      header_frame += "B2_order_param_" + elementString + "\t" +
+                      "alpha_occupancy_" + elementString + "\t" +
+                      "beta_occupancy_" + elementString + "\t";
     }
 
-    frame_ofs_ << "\n";
-    
-  }
-  frame_ofs_.close();
-}
-
-std::string Traverse::GetHeaderFrameString(const std::set<Element> &element_set) const 
-{
-  std::string header_frame = "steps\ttime\ttemperature\tenergy\t";
-  
-  // SRO Parameter
-  static const std::vector<std::string> order_list{"first", "second", "third"};
-  for (auto element1: element_set) {
-    for (auto element2: element_set) {
-      for (const auto &order : order_list) {
-        header_frame += "warren_cowley_" + order + "_" + element1.GetElementString()
-                            + "-" + element2.GetElementString() + "\t";
-      }
+    if (!header_frame.empty() && header_frame.back() == '\t')
+    {
+      header_frame.back() = '\n';
     }
+    return header_frame;
   }
 
-  // B2 Order Parameter
-  
-  for (auto element : element_set)
-  {
-    auto elementString = element.GetElementString();
-    header_frame += "B2_order_param_" + elementString + "\t" +
-                    "alpha_occupancy_" + elementString + "\t" +
-                    "beta_occupancy_" + elementString + "\t";
-                    
-  }
-
-
-  if (!header_frame.empty() && header_frame.back() == '\t') {
-    header_frame.back() = '\n';
-  }
-  return header_frame;
-}
-
-}    // namespace ansys
+} // namespace ansys
